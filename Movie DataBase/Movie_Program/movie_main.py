8edb5b05f91ac4149dfa34d86b10527875180274
Jet Tang Ro Starkweather <jstarkweather@sandiego.edu>
2025-09-11 17:46:52 -0700

    Added and connected to movie database to store watchlist

--- Movie DataBase/Movie_Program/movie_main.py
+++ Movie DataBase/Movie_Program/movie_main.py
@@ -1,17 +1,18 @@
 """Editing File"""
 from randomize_movie import randomize_movie
-from movie_functions import write_to_json_file
+from movie_functions import write_to_json_file, view_watchlist
 
 def main():
     """ Main Function """
     # Display options for user selections
-    print("Opening Watchlist...")
-    print("1. Search and Save Movie\n" \
-    "2. Randomize Movie from Watchlist\n" \
-    "3. View Watchlist (Coming Soon...)\n" \
-    "4. Exit")
+    print("Welcome!")
     stopper = False
     while not stopper:
+        print()
+        print("1. Search and Save Movie\n" \
+        "2. Randomize Movie from Watchlist\n" \
+        "3. View Watchlist\n" \
+        "4. Exit")
         try:
             # User input of a selection
             user = int(input(f"Select an option (e.g. 1):  "))
@@ -19,13 +20,12 @@ def main():
             if user == 1:
                 search_movie = input("Enter a movie: ")
                 write_to_json_file(search_movie)
-                print("Movie has been added to the watchlist.")
             # Allows user to randomize movie
             elif user == 2:
                 randomize_movie()
             # Allows user to view the current movie watchlist
             elif user == 3:
-                print("Coming Soon...")
+                view_watchlist()
             # Exits the program
             elif user == 4:
                 print("Come Again!")
